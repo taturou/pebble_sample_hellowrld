8c5caaf06acbf335ae2d0b36e08489a6ed1da74b
taturou <taturou@gmail.com>
2015-07-05 14:38:51 +0900

    日本語コメントを記述

--- src/hello_world.c
+++ src/hello_world.c
@@ -4,35 +4,37 @@ Window *window;
 TextLayer *text_layer;
 
 void handle_init(void) {
-	// Create a window and text layer
-	window = window_create();
-	text_layer = text_layer_create(GRect(0, 0, 144, 154));
-	
-	// Set the text, font, and text alignment
-	text_layer_set_text(text_layer, "Hi, I'm a Pebble!");
-	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-	
-	// Add the text layer to the window
-	layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+    // window と text_layer を作成
+    window = window_create();
+    text_layer = text_layer_create(GRect(0, 0, 144, 154));
 
-	// Push the window
-	window_stack_push(window, true);
-	
-	// App Logging!
-	APP_LOG(APP_LOG_LEVEL_DEBUG, "Just pushed a window!");
+    // 表示する文字を text_layer に設定
+    text_layer_set_text(text_layer, "Hi, I'm a Pebble!");
+    
+    // フォントと表示位置を設定
+    text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+    text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+
+    // text_layer を root_layer に重ねる
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_layer));
+
+    // window 
+    window_stack_push(window, true);
+
+    // ログを表示
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Just pushed a window!");
 }
 
 void handle_deinit(void) {
-	// Destroy the text layer
-	text_layer_destroy(text_layer);
-	
-	// Destroy the window
-	window_destroy(window);
+    // text_layer を破棄
+    text_layer_destroy(text_layer);
+
+    // window を破棄
+    window_destroy(window);
 }
 
 int main(void) {
-	handle_init();
-	app_event_loop();
-	handle_deinit();
+    handle_init();
+    app_event_loop();
+    handle_deinit();
 }
